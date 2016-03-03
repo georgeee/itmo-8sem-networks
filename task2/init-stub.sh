0f65e7826fce3dee7f6b1c5d51b0dd7d34b32a16
georgeee <george.agapov@gmail.com>
2016-03-04 02:12:03 +0300

    [Task2] separated virtual machines to different nat devices (internet access)

--- task2/init-stub.sh
+++ task2/init-stub.sh
@@ -34,7 +34,32 @@ function zebra_conf {
   echo '  link-detect'
 }
 
+function ensure_dev_up {
+  cnt=0
+  ret=1
+  while [[ $ret -ne 0 ]]; do
+    cnt=$((cnt+1))
+    ip link show $1 2>/dev/null 1>/dev/null
+    ret=$?
+    if [[ $cnt -gt 20 ]]; then
+      echo "Dev $1 is dead"
+      exit 3
+    fi
+    if [[ $ret -ne 0 ]]; then
+      sleep 0.5
+    fi
+  done
+  echo "Dev $1 is ready to up"
+}
+
+ensure_dev_up ens3
+ip link set ens3 up
+dhcpcd ens3
+
 if [[ $type == 'm' ]]; then
+  ensure_dev_up ens4
+  ensure_dev_up ens5
+  ensure_dev_up ens6
   ifconfig ens4 192.168.$p.1
   ifconfig ens5 192.168.$n.2
   ifconfig ens6 192.168.$id.1
@@ -47,5 +72,11 @@ if [[ $type == 'm' ]]; then
   ripd -d -f /etc/quagga/ripd.conf
   zebra -d -f /etc/quagga/zebra.conf
 else
+  ensure_dev_up ens4
   ifconfig ens4 192.168.$id.2
 fi
+
+ifconfig
+route -n
+
+echo "Machine $type$id launched"
--- task2/launch.sh
+++ task2/launch.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-base_img=$1
+base_img=$DIR/../base.img
+network_script=$DIR/../qemu-ifup.sh
 
 mkdir run-configs
 
 subnets=4
+mem=64
+
+launch_slave=true
+launch_master=true
+
+network_base="10.26"
+network_mask="255.255.255.0"
+network_min_i=10
+network_br_prefix=qemubr
+
+while test $# -gt 0
+do
+  case "$1" in
+    -m) mem=$2
+        shift
+        ;;
+    -s) subnets=$2
+        shift
+        ;;
+    -b) base_img=$2
+        shift
+        ;;
+    -no-m) launch_master=false
+        ;;
+    -no-s) launch_slave=false
+        ;;
+  esac
+  shift
+done
+
 
 function launch() {
   type=$1
@@ -13,14 +44,44 @@ function launch() {
   _i=$3
   echo "Launch $type $id $_i"
   i="$type$id"
-  rm -Rf run-configs/$i
-  mkdir -p run-configs/$i
-  create_config $type $id run-configs/$i
+  rm -Rf run-configs/$type$id
+  mkdir -p run-configs/$type$id
+  create_config $type $id run-configs/$type$id
+  get_args $type $id $_i
+  get_args $type $id $_i | launch_qemu $type $id $_i
+}
+
+function launch_qemu {
+  # # Set to the name of your bridge
+  # BRIDGE=br0
+  # 
+  # # Network information
+  # NETWORK=192.168.53.0
+  # NETMASK=255.255.255.0
+  # GATEWAY=192.168.53.1
+  # DHCPRANGE=192.168.53.2,192.168.53.254
+  type=$1
+  id=$2
+  _i=$3
   mac_prefix=02-00-00-00-`printf "%02x" $_i`
-  get_args $type $id $mac_prefix
-  get_args $type $id $mac_prefix | xargs \
-  	sudo qemu-system-i386 -enable-kvm -m 64 -hda "$base_img" -snapshot \
-	 -hdb fat:run-configs/$i -boot d -net tap -net nic,macaddr=$mac_prefix-01
+  i=$(($_i+$network_min_i))
+  script=run-configs/$type$id/host-net.sh
+  netscript_generate $i > "$script"
+  chmod +x "$script"
+  xargs \
+    sudo qemu-system-i386 -enable-kvm -m $mem -hda "$base_img" -snapshot -hdb fat:run-configs/$type$id -boot d \
+          -netdev tap,id=nic.$id,script="$script",downscript=no -device e1000,netdev=nic.$id,mac=$mac_prefix-01
+}
+
+function netscript_generate {
+  i=$1
+  echo "#!/bin/bash"
+  echo "export BRIDGE=$network_br_prefix$i"
+  echo "export NETWORK=$network_base.$i.0"
+  echo "export NETMASK=$network_mask"
+  echo "export GATEWAY=$network_base.$i.1"
+  echo "export DHCPRANGE=$network_base.$i.2,$network_base.$i.254" 
+  echo "$network_script "'$@'
 }
 
 function create_config() {
@@ -34,15 +95,15 @@ function create_config() {
 function get_args() {
   type=$1
   id=$2
+  _i=$3
+  mac_prefix=02-00-00-00-`printf "%02x" $_i`
   if [[ $id -eq 1 ]]; then
     p=$subnets$id
   else
     p=$((id-1))$id
   fi
   n=$id$((id%$subnets+1))
-  mac_prefix=$3
   if [[ $type == 'm' ]]; then
-#-netdev user,id=user.$p -device e1000,netdev=user.$p
      echo -netdev tap,id=user.$p -device e1000,netdev=user.$p,mac=$mac_prefix-02
      echo -netdev tap,id=user.$n -device e1000,netdev=user.$n,mac=$mac_prefix-03
      echo -netdev tap,id=user.$id -device e1000,netdev=user.$id,mac=$mac_prefix-04
@@ -53,7 +114,11 @@ function get_args() {
 
 i=1
 while [[ $i -le $subnets ]]; do 
-  launch m $i $((2*i)) &
-  launch s $i $((2*i+1)) &
+  if $launch_master; then
+    launch m $i $((2*i-1)) &
+  fi
+  if $launch_slave; then
+    launch s $i $((2*i)) &
+  fi
   i=$((i+1))
 done
