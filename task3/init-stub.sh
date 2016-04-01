198cb9316782a3a3a14e3c6f02dc8b4db481181b
georgeee <george.agapov@gmail.com>
2016-04-01 12:43:59 +0300

    Implemented task3 for ring topology

---
+++ task3/init-stub.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+###########################
+### Common part
+##########################
+
+id=%id%
+type=%type%
+machine_id=%machine_id%
+
+function ensure_dev_up {
+  cnt=0
+  ret=1
+  while [[ $ret -ne 0 ]]; do
+    cnt=$((cnt+1))
+    ip link show $1 2>/dev/null 1>/dev/null
+    ret=$?
+    if [[ $cnt -gt 20 ]]; then
+      echo "Dev $1 is dead"
+      exit 3
+    fi
+    if [[ $ret -ne 0 ]]; then
+      sleep 0.5
+    fi
+  done
+  echo "Dev $1 is ready to up"
+}
+
+##########################################
+## Task-specific part
+##########################################
+
+subnets=%subnets%
+enable_inet=%enable_inet%
+no_quagga=%no_quagga%
+
+if [[ $id -eq 1 ]]; then
+  p=$subnets$id
+else
+  p=$((id-1))$id
+fi
+n=$id$((id%$subnets+1))
+
+ifaces=()
+bridges=()
+indexes=()
+
+if [[ "$type" == 'm' ]]; then
+  ifaces=(ens4 ens5 ens6)
+  bridges=($p $n $id)
+  indexes=(2 1 1)
+else
+  ifaces=(ens4)
+  bridges=($id)
+  indexes=(2)
+fi
+
+function get_iface {
+  local i=$1
+  local iface=${ifaces[$i]}
+  echo $iface
+}
+function get_ipv6 {
+  local i=$1
+  local br=${bridges[$i]}
+  local ind=${indexes[$i]}
+  echo fc00:192:168:$br::$ind
+}
+function get_ipv4 {
+  local i=$1
+  local br=${bridges[$i]}
+  local ind=${indexes[$i]}
+  echo 192.168.$br.$ind
+}
+
+function ospf6d_conf {
+  echo '!'
+  echo 'password krispo'
+  echo 'enable password krispo'
+  echo 'log file /var/log/quagga/ospf6d.log'
+  echo "service advanced-vty"
+  echo '!'
+  echo "router ospf6"
+  echo "  router-id $machine_id.$machine_id.$machine_id.$machine_id"
+  local i=0
+  while [[ $i -lt ${#ifaces[*]} ]]; do
+    echo "  interface `get_iface $i` area 0.0.0.0"
+    i=$((i+1))
+  done
+  echo '!'
+  local i=0
+  while [[ $i -lt ${#ifaces[*]} ]]; do
+    echo "interface `get_iface $i`"
+    echo "  ipv6 ospf6 hello-interval 5"
+    echo "  ipv6 ospf6 dead-interval 10"
+    i=$((i+1))
+  done
+  echo '!'
+}
+
+function zebra_conf {
+  echo 'password krispo'
+  echo 'enable password krispo'
+  echo 'log file /var/log/quagga/zebra.log'
+  echo 'ip forwarding'
+  local i=0
+  while [[ $i -lt ${#ifaces[*]} ]]; do
+    echo "interface `get_iface $i`"
+    echo "  no ipv6 nd suppress-ra"
+    echo "  ipv6 address `get_ipv6 $i`/64"
+    echo "  ipv6 nd prefix `get_ipv6 $i`/64"
+    echo "  ipv6 nd ra-interval 10"
+    i=$((i+1))
+  done
+  echo "line vty"
+}
+
+if $enable_inet; then
+  ensure_dev_up ens3
+  ip link set ens3 up
+  dhcpcd ens3
+fi
+
+function init_ifaces {
+  local i=0
+  while [[ $i -lt ${#ifaces[*]} ]]; do
+    ensure_dev_up `get_iface $i`
+    ifconfig `get_iface $i` `get_ipv4 $i`/24
+    ifconfig `get_iface $i` inet6 add `get_ipv6 $i`/64
+    i=$((i+1))
+  done
+}
+
+init_ifaces
+
+if [[ $type == 'm' ]]; then
+  sysctl net.ipv4.ip_forward=1
+  sysctl net.ipv6.conf.default.forwarding=1
+  sysctl net.ipv6.conf.all.forwarding=1
+  chown -Rf quagga:quagga /var/log/quagga
+  ospf6d_conf | tee /etc/quagga/ospf6d.conf
+  zebra_conf | tee /etc/quagga/zebra.conf
+  if $no_quagga; then
+    echo "Quagga not launched (do it manually)"
+  else
+    ospf6d -d -f /etc/quagga/ospf6d.conf
+    zebra -d -f /etc/quagga/zebra.conf
+  fi
+else
+  route add -net 192.168.0.0/16 gw 192.168.$id.1
+  route -A inet6 add fc00:192:168::/48 gw fc00:192:168:$id::1 dev ens4
+fi
+
+ifconfig
+route -n
+
+echo "Machine $type$id launched"
+
+echo "export PS1='$type$id (inet: $enable_inet) >> '" > .bashrc
+
+bash --rcfile .bashrc
---
+++ task3/launch.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+### Launch.sh
+### Launches virtual machine cluster on several qemu instances
+
+### This script is to be adapted for concrete application
+### Basic idea: each machine in cluster has type and id (subnetwork id)
+
+
+APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $APP_DIR/../launch-base.sh
+
+### Arguments
+
+## Here you can customize arguments for your setup
+## Note that basic options like -m and -b would be parsed in launch-base.sh, so you don't have to worry about them
+
+subnets=4
+launch_slave=true
+launch_master=true
+ei=false #enable_internet
+no_quagga=false
+
+while test $# -gt 0
+do
+  case "$1" in
+    -no-m) launch_master=false
+        ;;
+    -no-s) launch_slave=false
+        ;;
+    -ei) ei=true
+        ;;
+    -nq) no_quagga=true
+        ;;
+    -s) subnets=$2
+        shift
+        ;;
+    *) break
+        ;;
+  esac
+  shift
+done
+
+### VM config generator
+## In this function you need to generate files for VM's environment
+## On VM start init.sh will be launched (if exists)
+
+# create_config <type> <id> <dir>
+
+function create_config() {
+  local type=$1
+  local id=$2
+  local dir=$3
+  local i=$4
+  cat "$APP_DIR/init-stub.sh" \
+    | sed "s/%enable_inet%/$ei/g" \
+    | sed "s/%id%/$id/g" \
+    | sed "s/%machine_id%/$i/g" \
+    | sed "s/%type%/$type/g" \
+    | sed "s/%subnets%/$subnets/g" \
+    | sed "s/%no_quagga%/$no_quagga/g" \
+    > "$dir/init.sh"
+  chmod +x "$dir/init.sh"
+}
+
+### QEMU additional arguments generator
+## Here you can generate additional arguments for QEMU, simply echoing them to stdout
+## Use generate_dev subroutine to setup necessary devices
+
+# get_args <type> <id> <machine_id>
+
+# generate_dev <machine_id> <id> <enable_inet>
+#
+# if enable_inet=true, internet would be appropriately set up
+#   otherwise (enable_inet=false) internet would be off for VM
+# id is an id of bridge, should be in range [1..254]
+# if two devices are connected to same bridge, they will share one network
+
+function get_args() {
+  local type=$1
+  local id=$2
+  local i=$3
+  if [[ $id -eq 1 ]]; then
+    local p=$subnets$id
+  else
+    local p=$((id-1))$id
+  fi
+  local n=$id$((id%$subnets+1))
+
+  echo "$i ($type$id) p=$p n=$n" >&2
+
+  generate_dev $i $id $ei
+  if [[ $type == 'm' ]]; then
+     generate_dev $i $p
+     generate_dev $i $n
+     generate_dev $i $id $ei
+  else
+     generate_dev $i $id $ei
+  fi
+}
+
+### Main section
+## This section launches all VMs
+
+## Use launch subroutine to launch VMs
+## Please note, that machine_id values should be unique
+## As well as pairs {type, id}
+
+# launch <type> <id> <machine_id>
+
+id=1
+i=1
+while [[ $id -le $subnets ]]; do 
+  if $launch_master; then
+    launch m $id $((2*i-1)) &
+  fi
+  if $launch_slave; then
+    launch s $id $((2*i)) &
+  fi
+  i=$((i+1))
+  id=$((id+1))
+done
