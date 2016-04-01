42f3d580912181a54eba3905057da539b136ed8e
Martoon <Martoon_00@mail.ru>
2016-04-01 15:29:40 +0300

    lol3

--- network-gen/src/EnvGraph.hs
+++ network-gen/src/EnvGraph.hs
@@ -16,17 +16,23 @@ instance Show EnvGraph where
             '\t' : entry ++ "\n"
 
         nodesList :: Node -> String
-        nodesList = liftM3 (printf "%s [label=\"%s\", color=\"%s\"];") id id nodeColor
+        nodesList = liftM3 (printf "%s [label=\"%s\", color=\"%s\", style=filled];") id id nodeColor
        
         edgesList :: (BridgeId, (Node, Node)) -> String
-        edgesList (bid, (n1, n2)) = printf "%s -- %s [label=\"%d\"];" n1 n2 bid
+        edgesList (bid, (n1, n2)) = printf "%s -- %s [label=\"%d\", color=\"%s\"];" n1 n2 bid (edgeColor bid)
         
+        nodeColor :: Node -> String
         nodeColor n 
             | n `elem` envClients e 
            && n `elem` envServers e  =  "cyan3"
             | n `elem` envClients e  =  "green"
             | n `elem` envServers e  =  "blue"
-            | otherwise              =  "black"
+            | otherwise              =  "gray"
+
+        edgeColor :: BridgeId -> String
+        edgeColor  
+            | length (envBridges e) <= 8  =  printf "/accent8/%d"
+            | otherwise                   =  const "black"
         
 instance Output EnvGraph where
     defName _  =  "graph.dot" 
